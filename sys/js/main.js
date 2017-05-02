e56a9ad179cb439cb79ebb9cb377e29b28226ed3
Shane Gadsby <Shane.Gadsby@usq.edu.au>
2017-05-02 14:01:50 +1000

    adding to master

---
+++ sys/js/main.js
@@ -0,0 +1,25 @@
+angular.module('111111 - 111111', [])
+
+/*The master controller*/
+.controller('master', function master($scope) {
+	m = $scope
+})
+
+/*Turns off the ng-scope, et al. debug classes*/
+.config(['$compileProvider', function ($compileProvider) {
+	$compileProvider.debugInfoEnabled(false);
+}])
+
+
+/*Directives*/
+.directive('touch', function () {
+	return function (scope, element, attrs) {
+		element.bind('pointerdown', function () {
+			try {
+				scope.$apply(attrs.touch)
+			} catch (e) {
+				if (typeof console !== "object") console.log(e)
+			}
+		})
+	}
+})
