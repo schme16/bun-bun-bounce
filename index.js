a7d8477dc025d9d3ff49da8398b16ba0950bd1a9
Shane Gadsby <Shane.Gadsby@usq.edu.au>
2017-05-02 15:30:37 +1000

    added fb stuff and cors

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ express = require('express')
 app = express()
 
 app.use(require('compression')())
+app.use(require('cors')())
+app.use(require('body-parser')({extended: true}))
 
 app.use(express.static(__dirname + '/public'))
+app.post(express.static(__dirname + '/public'))
 
 
 
--- public/sys/js/main.js
+++ public/sys/js/main.js
@@ -6,6 +6,37 @@ window.fbAsyncInit = function() {
 	});
 
 // ADD ADDITIONAL FACEBOOK CODE HERE
+
+
+		// Place following code after FB.init call.
+
+		function onLogin(response) {
+		  if (response.status == 'connected') {
+		    FB.api('/me?fields=first_name', function(data) {
+		      var welcomeBlock = document.getElementById('fb-welcome');
+		      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+		    });
+		  }
+		}
+
+		FB.getLoginStatus(function(response) {
+		  // Check login status on load, and if the user is
+		  // already logged in, go directly to the welcome message.
+		  if (response.status == 'connected') {
+		    onLogin(response);
+		  } else {
+		    // Otherwise, show Login dialog first.
+		    FB.login(function(response) {
+		      onLogin(response);
+		    }, {scope: 'user_friends, email'});
+		  }
+		});
+
+
+
+
+
+
 };
 
 (function(d, s, id){
@@ -16,6 +47,8 @@ window.fbAsyncInit = function() {
 	js.src = "//connect.facebook.net/en_US/sdk.js";
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+
 
 
 
